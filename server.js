d3486bce44562e8ca5fea0a5ebd746c0f060f97d
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-05-31 06:40:13 +0700

    finish backend

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+const passport = require("passport")
+const cookieSession = require("cookie-session")
+const passportSetup = require("./passport")
+const authRoute = require("./routes/auth")
+const app = express();
+
+// ตั้งค่า Google OAuth
+const clientID = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+app.use(
+    cookieSession({
+        name: "session",
+        keys: ["cyberwolve"],
+        maxAge: 24 * 60 * 60 * 100,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+    })
+);
+
+app.use("/auth",authRoute);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listen on port ${port}...`));
